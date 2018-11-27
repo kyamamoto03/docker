2f4d7f1e758af14576b45176b399d0d68a415a30
Kohji Yamamoto <k.yamamoto03@iforcom.jp>
2018-11-27 09:36:36 +0900

    追加

---
+++ HelloMongo/MongoTest/MongoTest/Model/Todo.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MongoTest.Model
+{
+    public class Todo
+    {
+        public ObjectId _id { get; set; }
+        public int TodoId { get; set; }
+        public string TodoName { get; set; }
+    }
+}
---
+++ HelloMongo/MongoTest/MongoTest/Pages/Insert.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MongoDB.Driver;
+using MongoTest.Model;
+
+namespace MongoTest.Pages
+{
+    public class InsertModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnPost(Todo todo)
+        {
+            var connectionString = "mongodb://root:root@mongo";
+            var database = "TodoDB";
+            var _client = new MongoClient(connectionString);
+            var _database = _client.GetDatabase(database);
+
+            var data = _database.GetCollection<Todo>("Todo");
+
+            data.InsertOne(todo);
+
+            return Redirect("/");
+        }
+    }
+}
