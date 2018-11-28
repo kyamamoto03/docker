dd8a4df8b868e3a17c742e880b24cec997091022
Kohji Yamamoto <k.yamamoto03@iforcom.jp>
2018-11-28 13:38:44 +0900

    MongoWith追加

---
+++ MongoWith/MongoTest/Pages/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MongoDB.Driver;
+using MongoTest.Model;
+
+namespace MongoTest.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public List<Todo> Todos;
+
+        public void OnGet()
+        {
+            Todos = GetAll();
+        }
+
+        private List<Todo>GetAll()
+        {
+            var connectionString = "mongodb://localhost";
+            var database = "TodoDB";
+            var _client = new MongoClient(connectionString);
+            var _database = _client.GetDatabase(database);
+
+            var data = _database.GetCollection<Todo>("Todo");
+
+            var rets = data.Find(_ => true).ToList();
+            return rets;
+        }
+    }
+}
